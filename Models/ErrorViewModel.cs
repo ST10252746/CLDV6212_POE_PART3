14ed2c5c7168f9450539985d31b87be09470a184
ST10252746 <st10252746@vcconnect.edu.za>
2024-11-06 13:26:57 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10252746_CLDV6212_POE_PART3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
